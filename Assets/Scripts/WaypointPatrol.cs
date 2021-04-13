3165bfb7e6204ce99d33c56fd07926dd410c0a14
bclauson <clauson.branden@gmail.com>
2021-04-13 12:01:04 -0700

    enemies ghosts fix

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -5,27 +5,22 @@ using UnityEngine.AI;
 
 public class WaypointPatrol : MonoBehaviour
 {
-
     public NavMeshAgent navMeshAgent;
     public Transform[] waypoints;
-    int m_CurrentWaypointIndex;
 
+    int m_CurrentWaypointIndex;
 
     void Start()
     {
         navMeshAgent.SetDestination(waypoints[0].position);
-
-        
     }
-
 
     void Update()
     {
-        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
         {
             m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
-            navMeshAgent.SetDestination (waypoints[m_CurrentWaypointIndex].position);
-
+            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
         }
     }
 }
